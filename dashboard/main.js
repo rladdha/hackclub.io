7cf0174542fbd84a2b8e706083623b0a2baccd05
Ronak Laddha <ronak.laddha@gmail.com>
2017-07-27 20:36:46 +0000

    Add api's to Dashboard

---
+++ dashboard/main.js
@@ -0,0 +1,54 @@
+function loadDate() {
+  var currentDate = new Date();
+  var dateString = currentDate.toString()
+                     .split(" ")
+                     .splice(0, 4)
+                     .join(" ");
+
+  /*global $*/
+  $("#date").text(dateString);
+}
+
+function loadWeather() {
+  var weather = $("#weather");
+  var url = "https://api.forecast.io/forecast/";
+  var apiKey = "a337bb6dd172b91890e9a86cfb2762a9";
+
+  function success(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
+      weather.text("Based on your current location, it is " + data.currently.temperature + "° F right now");
+    });
+  }
+
+  function error() {
+    alert("Unable to retrieve your location for weather");
+  }
+
+  /*global navigator*/
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  weather.text("fetching weather...");
+}
+
+function loadNews() {
+  var news = $("#news");
+  var url = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=";
+  var apiKey = "d09a5d83e2fd4abf94d9f7c6c3059c96";
+
+  $.getJSON(url + apiKey, function(data) {
+    var titles = data.articles.map(function(articles) {
+      return "<a href='" + articles.url + "'>" + articles.title + "</a>";
+    });
+
+    news.html(titles.join("<br><br>"));
+  });
+
+  news.text("fetching news...");
+}
+
+loadDate();
+loadWeather();
+loadNews();
